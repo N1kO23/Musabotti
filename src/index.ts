35d9409401c129eb4496a3cf175a6d4b1abab897
N1kO23 <niko.huuskonen.00@gmail.com>
2025-05-22 23:20:17 +0300

    remove await due to race condition with vc client

--- src/index.ts
+++ src/index.ts
@@ -140,7 +140,7 @@ client.on(Events.MessageCreate, async (message) => {
       try {
         const parsedArgs = commandInstance.parseArgs(args);
         try {
-          await commandInstance.execute(shoukaku, client, context, parsedArgs);
+          commandInstance.execute(shoukaku, client, context, parsedArgs);
         } catch (error) {
           message.reply(error?.toString() ?? "Error executing command");
           console.error(error);
@@ -183,12 +183,7 @@ client.on(Events.InteractionCreate, async (interaction) => {
         context,
       });
       try {
-        await commandInstance.execute(
-          shoukaku,
-          client,
-          context,
-          args as IArgument[]
-        );
+        commandInstance.execute(shoukaku, client, context, args as IArgument[]);
       } catch (error) {
         context.reply(error?.toString() ?? "Error executing command");
         console.error(error);
